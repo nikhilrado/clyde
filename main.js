790511b99c7d7b9c6ea61f628b335263f59d8ab5
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2022-03-27 21:47:48 -0400

    Fixed email issue by implementing link shortening -improved code comments -link shortener works via firebase dynamic links CU-29g4m7w

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function test(){
   loadData()
   Logger.log(uniqueUserNamesList)
   for (var i = 0; i < uniqueUserNamesList.length-1; i++) {
-    Logger.log(uniqueUserNamesList[i])
+    //Logger.log(uniqueUserNamesList[i])
     //Logger.log(mergeToTable(rowMerge(uniqueUserNamesList[i])))
     sendUpdateEmail(uniqueUserNamesList[i])
   }
@@ -46,6 +46,7 @@ function sendUpdateEmail(lookupUserName) {
   Logger.log(htmlBody);
 }
 
+// returns a html "progress bar" out of colored l characters
 function getLameProgressBar(part, whole){
   var bar = ""
   var percent = (part/whole)*100
@@ -65,6 +66,7 @@ function getLameProgressBar(part, whole){
 
 }
 
+// returns the number of hours required for each person
 function getRequiredHours(lookupUserName) {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Settings");
   var startRow = 3; // First row of data to process
@@ -73,7 +75,7 @@ function getRequiredHours(lookupUserName) {
   var dataRange = sheet.getRange(startRow, 1, numRows, 2);
   // Fetch values for each row in the Range.
   var data = dataRange.getValues();
-  Logger.log(data)
+
   for (var i = 0; i < data.length; i++) {
     var row = data[i];
     var userName = row[0];
@@ -85,7 +87,7 @@ function getRequiredHours(lookupUserName) {
   return(defaultRequiredHours);
 }
 
-//adds hours from a username, returns value formatted to two decimal places
+// adds hours from a username, returns value formatted to two decimal places
 function addHours(lookupUserName) {
 
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Data Log");
@@ -116,6 +118,9 @@ function addHours(lookupUserName) {
   return totalHours.toFixed(2)
 }
 
+// adds and categorizes the number of hours from a 2d array
+// adds a row of this users's stats to statsMerge for later processing 
+// decimal value of total hours that a user has completed
 function addHoursFromMerge(merge) {
   var today = new Date()
   var month = today.getMonth()
@@ -173,7 +178,7 @@ function addHoursFromMerge(merge) {
     }
   }
 
-  //this section rounds our numerical values to 3 decimal places which compresses outputted data
+  // this section rounds our numerical values to 3 decimal places which compresses outputted data
   var rowToAppend = [userName,totalHours,tutoringHours,meetingHours,otherHours,tutoringHoursLastMonth,meetingHoursLastMonth,otherHoursLastMonth,firstName,lastName]
   for (var i = 0; i < rowToAppend.length; i++){
     if (!isNaN(rowToAppend[i])){
@@ -185,6 +190,7 @@ function addHoursFromMerge(merge) {
   return totalHours
 }
 
+// selects and groups form entries from a single username, and returns a list with the submissions
 function rowMerge(lookupUserName) {
   var mergedArray = [];
 
@@ -216,6 +222,7 @@ function rowMerge(lookupUserName) {
   return mergedArray
 }
 
+// takes a 2d array, with the index of the column containing the date, and returns an html table with dates formatted
 function mergeToTable(merge,dateIndex) { 
   //console.log(merge)
   s = " style='border:1px solid'" //style for td, th
@@ -243,11 +250,13 @@ function mergeToTable(merge,dateIndex) {
   return html
 }
 
+// generates a pre-filled link to the google form with 
 function generatePreFilledLink(userName,firstName,lastName) {
   //old link return "https://docs.google.com/forms/d/e/1FAIpQLSdk8TmquLj2xm1EkbQCgMAbtk_SIpX0XuPDiecqc2HKoKAlFw/viewform?usp=pp_url&entry.76228359=" + userName + "&entry.1315765559=" + firstName + "&entry.1356290163=" + lastName
  return "https://docs.google.com/forms/d/e/1FAIpQLSfBKs37oy8vdRuu2OaLCV-KP6LF7bFPqrAFC5saOai2sJKy8A/viewform?usp=pp_url&entry.1129123924=" + userName + "&entry.624029689=" + firstName + "&entry.172496163=" + lastName;
 }
 
+// load initial data from the spreadsheet form log
 function loadData() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Data Log");
   var startRow = 2; // First row of data to process
@@ -265,7 +274,9 @@ function loadData() {
   //uniqueUserNamesList.remove("");
 }
 
-function addRowAtDate(merge){ //adds a line into the merge at where the previous month starts
+// TODO implement this into code
+// adds a line into the merge at where the previous month starts
+function addRowAtDate(merge){
   var today = new Date();
   var month = today.getMonth()
   //if (prevMonth == -1){prevMonth = 0}
@@ -282,6 +293,7 @@ function addRowAtDate(merge){ //adds a line into the merge at where the previous
   return merge
 }
 
+// returns a friendly greeting based on the day
 function getGreeting(){
   var today = new Date()
   var dayNumber = today.getDay()
@@ -289,21 +301,17 @@ function getGreeting(){
   const ADJECTIVES_LIST = [["Super","Splendid","Serene","Spectacular"],["Marvelous","Magical","Magnificent"],["Terrific","Top Notch"],["Wonderful"],["Terrific","Top Notch"],["Fabulous","Fantastic","Festive"],["Super","Splendid","Serene","Spectacular"]]
 
   var adjective = ADJECTIVES_LIST[dayNumber][Math.floor(Math.random()*ADJECTIVES_LIST[dayNumber].length)]
-  //var adjective = [Math.floor(Math.random()*adjectivesList[dayNumber].length)]
-
   var greeting = "I hope you are having a " + adjective + " " + DAYS_OF_WEEK[dayNumber] +"!"
 
-  //Logger.log(greeting)
   return(greeting)
-
 }
 
-//the following code is for the teacher email
+// returns a link to a chart that contains the hours data of all students
 function getHoursChart(){
-  statsMerge = statsMerge.sort(function(a,b) { //sorts the statsMerge by number of hours which is column 2 (index 1)
+  statsMerge = statsMerge.sort(function(a,b) { // sorts the statsMerge by number of hours which is column 2 (index 1)
     return a[1] - b[1];
   });
-  statsMerge.reverse() //reverses the array so that it is highest to lowest
+  statsMerge.reverse() // reverses the array so that it is highest to lowest
   var names = []
   var meetingHours = []
   var tutoringHours = []
@@ -354,14 +362,15 @@ function getHoursChart(){
   // rough approximation of image height with more names, in order for all names to be visible
   chartImageHeight = 450 + (names.length-25)*18
 
-  chartSlug = '{type:"horizontalBar",data:{labels:'+namesJSON+',datasets:[{label:"",backgroundColor:"rgb(52, 41, 255)",stack:"Stack0",data:'+meetingHours+',},{label:"Meetings",backgroundColor:"rgb(76, 66, 255)",stack:"Stack0",data:'+meetingHoursLastMonth+',},{label:"",backgroundColor:"rgb(54,162,235)",stack:"Stack0",data:'+tutoringHours+',},{label:"Tutoring",backgroundColor:"rgb(54,162,200)",stack:"Stack0",data:'+tutoringHoursLastMonth+',},{label:"",backgroundColor:"rgb(75,192,192)",stack:"Stack0",data:'+otherHours+',},{label:"Other",backgroundColor:"rgb(75,192,150)",stack:"Stack0",data:'+otherHoursLastMonth+',},{label:"HoursLeftHide",backgroundColor:"rgb(200,200,200)",stack:"Stack0",data:'+hoursLeft+',}],},options:{legend:{labels:{filter: function(item, chart){return !item.text.includes("Hide");}}},title:{display:true,text:"CSHS Hours",},tooltips:{mode:"index",intersect:false,},responsive:true,scales:{xAxes:[{stacked:true,scaleLabel:{display:true,labelString:"Hours",},},],yAxes:[{stacked:true,gridLines:{display:false },},],},},}'
+  chartSlug = '{type:"horizontalBar",data:{labels:'+namesJSON+',datasets:[{label:"",backgroundColor:"rgb(52, 41, 255)",stack:"Stack0",data:'+meetingHours+',},{label:"Meetings",backgroundColor:"rgb(76, 66, 255)",stack:"Stack0",data:'+meetingHoursLastMonth+',},{label:"",backgroundColor:"rgb(54,162,235)",stack:"Stack0",data:'+tutoringHours+',},{label:"Tutoring",backgroundColor:"rgb(54,162,200)",stack:"Stack0",data:'+tutoringHoursLastMonth+',},{label:"",backgroundColor:"rgb(75,192,192)",stack:"Stack0",data:'+otherHours+',},{label:"Other",backgroundColor:"rgb(75,192,150)",stack:"Stack0",data:'+otherHoursLastMonth+',},{label:"HoursLeftHide",backgroundColor:"rgb(200,200,200)",stack:"Stack0",data:'+hoursLeft+',}],},options:{legend:{labels:{filter:function(item,chart){return !item.text.includes("Hide");}}},title:{display:true,text:"CSHS Hours",},tooltips:{mode:"index",intersect:false,},responsive:true,scales:{xAxes:[{stacked:true,scaleLabel:{display:true,labelString:"Hours",},},],yAxes:[{stacked:true,gridLines:{display:false },},],},},}'
 
-//{xAxes:[{stacked:true,scaleLabel: {display: true,labelString: "Hours",},},],yAxes
+  //{xAxes:[{stacked:true,scaleLabel: {display: true,labelString: "Hours",},},],yAxes
 
   return ("https://quickchart.io/chart?height="+chartImageHeight+"&c="+chartSlug)
 
 }
 
+// will loop through teachers in the spreadsheet worksheet "Settings" and send them each a report
 function sendReports(){
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Settings");
   var startRow = 3; // First row of data to process
@@ -371,24 +380,58 @@ function sendReports(){
   // Fetch values for each row in the Range.
   var data = dataRange.getValues();
 
-  for (var i = 0; i < data.length; ++i) {
+  var hoursChartLink = shortenUrl(getHoursChart())
+  shortenUrl("https://google.com")
+
+  for (var i = 0; i < data.length; i++) {
     row = data[i];
     teacherEmail = row[3];
     teacherName = row[4];
     if (teacherName != "" && teacherEmail != ""){
-      sendTeacherEmail(teacherEmail,teacherName);
+      sendTeacherEmail(teacherEmail,teacherName, hoursChartLink);
     }
   }
   return(defaultRequiredHours);
 }
 
-function sendTeacherEmail(email, name){
-  Logger.log("teacher email started")
-  var htmlBody = "Hello " + name + ",<br><br>"+getGreeting()+"<br><br>Here's how your students have been doing:<br><br>" + "<img src='"+ getHoursChart() +"' alt='Hours Chart'>"
+// sends teachers the report email
+function sendTeacherEmail(email, name, hoursChartLink){
+  var htmlBody = "Hello " + name + ",<br><br>"+getGreeting()+"<br><br>Here's how your students have been doing:<br><br>" + "<img src='"+ hoursChartLink +"' alt='Hours Chart'><br>If you have trouble viewing the chart click <a href='"+hoursChartLink+"'>here</a>"
   var subject = "Here's how your students have been doing..."
 
   if (SEND_EMAILS){
     MailApp.sendEmail({to: email,name: "Clyde",subject: subject,htmlBody: htmlBody});
   }
   Logger.log(htmlBody)
+}
+
+// creates a firebase short link to decrease chance of gmail stripping image src and link href properties
+// shortener only allows quickchart image links
+// function modified from https://github.com/amitwilson/GoogleAppsScript/blob/master/LinkShortner
+function shortenUrl(longLink) {
+  // gets the firebase api key from script properties
+  var firebaseWebAPIKey = PropertiesService.getScriptProperties().getProperty('FIREBASE_SHORTERNER_API_KEY');
+  // the following line would set the api key at some point
+  //PropertiesService.getScriptProperties().setProperty('FIREBASE_SHORTERNER_API_KEY', '')
+
+  var apiUrl = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=' + firebaseWebAPIKey;
+  var prefix = 'https://clyde.page.link'; // domain should be registered in firebase
+  var formData = {
+    "dynamicLinkInfo": {
+      "domainUriPrefix": prefix,
+      "link": longLink 
+    },
+    "suffix": {
+      "option": "UNGUESSABLE"
+    }
+  }
+  var options = {
+    'method'  : 'POST',
+    'headers' : {'Content-Type' : 'application/json'},
+    'payload' : JSON.stringify(formData)
+  };  
+  var response = UrlFetchApp.fetch(apiUrl, options);
+  response = JSON.parse(response.getContentText());
+  var shortLink = response.shortLink;
+  return shortLink;
 }
