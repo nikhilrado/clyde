ab6473a542bad1e386d89c704e293db5093aece1
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2022-08-07 15:05:16 -0400

    formatting improvements

--- classroom.js
+++ classroom.js
@@ -23,7 +23,7 @@ function listCourses() {
     for (const course in courses) {
       //Logger.log('%s (%s)', courses[course].name, courses[course].id);
       // tests if user has teacher permissions, and only displays those classes
-      if (courses[course].enrollmentCode){
+      if (courses[course].enrollmentCode) {
         console.log(courses[course])
         returnList.push(courses[course])
       }
@@ -35,27 +35,27 @@ function listCourses() {
   return returnList
 }
 
-function test343(){
+function test343() {
 
-  addToRange(getPeople(course_id,"students"),"A101:C199")
+  addToRange(getPeople(course_id, "students"), "A101:C199")
   Logger.log("yo")
 }
 
 course_id = "323607317477"
-function getPeople(courseId, personType){
+function getPeople(courseId, personType) {
   returnList = []
-  if (personType == 'students'){var people = Classroom.Courses.Students.list(courseId)}
-  else if (personType == 'teachers'){var people = Classroom.Courses.Teachers.list(courseId)}
-  else {console.error("personType must be set to 'teachers' or 'students'")}
+  if (personType == 'students') { var people = Classroom.Courses.Students.list(courseId) }
+  else if (personType == 'teachers') { var people = Classroom.Courses.Teachers.list(courseId) }
+  else { console.error("personType must be set to 'teachers' or 'students'") }
   //var students = course.students()
   console.log(people[personType][5])
-  for (var i = 0; i < people[personType].length; i++){
+  for (var i = 0; i < people[personType].length; i++) {
     //students["students"][i])
     person = people[personType][i]['profile']
     studentFirstName = person['name']['givenName']
     studentLastName = person['name']['familyName']
-    studentUserName = person['emailAddress'].replace("@"+EMAIL_DOMAIN,"") //keeps domain if different from set domain
-    studentInfo = [studentFirstName,studentLastName,studentUserName]
+    studentUserName = person['emailAddress'].replace("@" + EMAIL_DOMAIN, "") //keeps domain if different from set domain
+    studentInfo = [studentFirstName, studentLastName, studentUserName]
     //console.log(studentInfo)
     //console.log(i)
     returnList.push(studentInfo)
@@ -68,9 +68,9 @@ function getPeople(courseId, personType){
 
 // this function takes in data with a cell, and the column the bounding row. 
 // the function will calulate the row number of the second cell based on data size.
-function setRange(data,range){
+function setRange(data, range) {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Settings");
-  
+
   var regex = /^([a-zA-Z]+)(\d+):([a-zA-Z]+)(\d+)$/g
   range = regex.exec(range)
   console.log(range)
@@ -79,16 +79,16 @@ function setRange(data,range){
   //console.log(parseInt(rangeCell1.match(/\d+/g)))
   console.log(data)
   //console.log(`${rangeCell1}:${rangeCol2}${parseInt(rangeCell1.match(/\d+/g))+data.length-1}`)
-  if (parseInt(range[2])+data.length-1 > range[4]){
+  if (parseInt(range[2]) + data.length - 1 > range[4]) {
     console.error("data does not fit")
     return
   }
-  var range = sheet.getRange(`${range[1]+range[2]}:${range[3]}${parseInt(range[2])+data.length-1}`);
+  var range = sheet.getRange(`${range[1] + range[2]}:${range[3]}${parseInt(range[2]) + data.length - 1}`);
   range.setValues(data);
 }
 
 // adds data to a range without overwriting previous rows
-function addToRange(data,range){
+function addToRange(data, range) {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Settings");
 
   var regex = /^([a-zA-Z]+)(\d+):([a-zA-Z]+)(\d+)$/g
@@ -98,17 +98,17 @@ function addToRange(data,range){
   // finds last filled row in range (can't use binary cause unsorted/blank)
   // currently only checks first column for data
   existingData = sheet.getRange(range[0]).getValues()
-  for (var i = existingData.length - 1; i >= 0; i--){
+  for (var i = existingData.length - 1; i >= 0; i--) {
     console.log(existingData[i])
-    if (existingData[i][0] == '' && existingData[i-1][0] != ''){
+    if (existingData[i][0] == '' && existingData[i - 1][0] != '') {
       var insertAt = i
       break
     }
   }
   console.log(insertAt)
   console.log(data)
-  
-  var newRangeString = `${range[1]+(parseInt(range[2])+insertAt)}:${range[3]}${range[4]}`
+
+  var newRangeString = `${range[1] + (parseInt(range[2]) + insertAt)}:${range[3]}${range[4]}`
   console.log(newRangeString)
   setRange(data, newRangeString)
 }
